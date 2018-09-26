4887f218d75d0041348f7688d744bfa0abccd7eb
Franz Knobel <knobelf@student.ethz.ch>
2018-09-26 22:34:58 +0200

    created skeleton

--- task0/example.py
+++ task0/example.py
@@ -1,3 +1,3 @@
-from importExport import write_to_csv_from_vector, read_csv_to_matrix, read_hdf_to_matrix
+# from importExport import write_to_csv_from_vector, read_csv_to_matrix, read_hdf_to_matrix
 
 print("this is an example file, hello")
