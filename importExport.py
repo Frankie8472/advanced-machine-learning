61e5dfd0714dd299f4fde30b52d0515765459c4a
Franz Knobel <knobelf@student.ethz.ch>
2018-09-26 22:32:33 +0200

    created skeleton

---
+++ importExport.py
@@ -0,0 +1,16 @@
+import numpy as np
+from pandas import DataFrame, read_csv, read_hdf
+
+
+def read_hdf_to_matrix(filename):
+    data = read_hdf("input/" + filename)
+    return data.as_matrix(), data.index
+
+
+def read_csv_to_matrix(filename, index_name):
+    data = read_csv("input/" + filename, index_col=index_name)
+    return data.as_matrix(), data.index
+
+
+def write_to_csv_from_vector(filename, index_col, vec):
+    return DataFrame(np.c_[index_col, vec]).to_csv("output/" + filename, index=False, header=["Id", "y"])
