cd7322d4bbac3aae46dc64d994e58c9c01a16624
Franz Knobel <knobelf@student.ethz.ch>
2018-09-26 22:39:26 +0200

    made some unnecessary changes

--- task0/example.py
+++ task0/example.py
@@ -1,3 +1,3 @@
 # from importExport import write_to_csv_from_vector, read_csv_to_matrix, read_hdf_to_matrix
 
-print("this is an example file, hello")
+print("Make the plan, execute the plan, expect the plan to go off the rails... throw away the plan")
