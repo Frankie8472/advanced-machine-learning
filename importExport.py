dba00f0aa61a06fbda0e1a44f7df8daf64558774
Frankie <knobelf@student.ethz.ch>
2018-09-27 14:34:03 +0200

    added task0 and my file (in progress)

--- importExport.py
+++ importExport.py
@@ -14,3 +14,9 @@ def read_csv_to_matrix(filename, index_name):
 
 def write_to_csv_from_vector(filename, index_col, vec):
     return DataFrame(np.c_[index_col, vec]).to_csv("output/" + filename, index=False, header=["Id", "y"])
+
+
+def split_into_x_y(data_set):
+    y = data_set[:, 0]
+    x = data_set[:, 1:]
+    return x, y
