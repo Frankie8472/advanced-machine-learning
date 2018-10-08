d4251568d11732cc85ccf5c409d2139b055f2a79
Nicola Rueegsegger <runicola@student.ethz.ch>
2018-10-08 14:38:52 +0200

    Crack hard baseline, final commit

--- task1/task1_nicu.py
+++ task1/task1_nicu.py
@@ -1,12 +1,9 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import RidgeCV
-#from sklearn.linear_model import LassoCV
-#from sklearn.linear_model import ElasticNetCV
 from sklearn.impute import SimpleImputer
-from sklearn.decomposition import PCA
-from sklearn.feature_selection import SelectPercentile, f_regression
-from sklearn.preprocessing import StandardScaler
+from sklearn.feature_selection import SelectKBest, f_regression
+from sklearn.preprocessing import QuantileTransformer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 
@@ -22,27 +19,18 @@ X_train.columns = X_train_missing.columns
 X_test = pd.DataFrame(imp.transform(X_test_missing))
 X_test.columns = X_test_missing.columns
 
-# Standardize features
-scaler = StandardScaler()
+# Standardize features, takes care of outliers
+scaler = QuantileTransformer()
 X_train = pd.DataFrame(scaler.fit_transform(X_train))
 X_test = pd.DataFrame(scaler.transform(X_test))
 
-#TODO Outlier removal
-
-# Run a PCA, keep PC which explain 95% of the variance
-#pca = PCA(0.95)
-#X_train = pd.DataFrame(pca.fit_transform(X_train))
-#X_test = pd.DataFrame(pca.transform(X_test))
-
 # Do a feature selection
-selector = SelectPercentile(f_regression, percentile=35)
+selector = SelectKBest(f_regression, k=222)
 X_train = pd.DataFrame(selector.fit_transform(X_train, Y_train))
 X_test = pd.DataFrame(selector.transform(X_test))
 
-
 # Split data for validation
 #X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size=0.25, random_state=42)
-
 
 # Perform training with an Elastic Net and cross-validation
 alphas = [0.00001, 0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0]
@@ -53,8 +41,7 @@ reg.fit(X_train, Y_train)
 #Y_out = reg.predict(X_val)
 #print(r2_score(Y_val, Y_out))
 
-
 # Get results and print to file
 Y_results = reg.predict(X_test)
 result = pd.DataFrame(data=Y_results, index=X_test.index.values)
-result.to_csv("./output/output_nicu_version_4.csv", index=True, index_label="id", header=['y'])
+result.to_csv("./output/output_nicu_version_5.csv", index=True, index_label="id", header=['y'])
