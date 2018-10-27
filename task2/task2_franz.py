be0b9152aef515dcc301a20c8328f59e40d71d8c
Franz Knobel <knobelf@student.ethz.ch>
2018-10-28 01:57:26 +0200

    Omae wa mou shindeiru

--- task2/task2_franz.py
+++ task2/task2_franz.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import StratifiedKFold, GridSearchCV
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import balanced_accuracy_score, make_scorer
 from sklearn.preprocessing import QuantileTransformer
-from sklearn.feature_selection import f_classif, SelectPercentile
+from sklearn.feature_selection import f_classif, SelectPercentile, SelectKBest
 from sklearn.svm import SVC
 
 """ 
@@ -43,7 +43,8 @@ def scale_features(data_train, data_test):
 
 
 def remove_unimportant_features(data_train, data_test, y):
-    ft = SelectPercentile(f_classif, percentile=70)
+    ft = SelectPercentile(percentile=70)
+    #ft = SelectKBest(k=750)
     ft.fit(data_train, y)
     data_train = ft.transform(data_train)
     data_test = ft.transform(data_test)
@@ -60,23 +61,25 @@ def read_data():
 def evaluate(X_train, y_train, iid):
     estimator = [
         ('qt', QuantileTransformer()),
-        ('sp', SelectPercentile()),
+        #('sp', SelectPercentile()),
+        ('sk', SelectKBest()),
         ('svc', SVC())
     ]
 
     param_grid = {
-        'qt__n_quantiles': [10, 100, 1000, 10000],
-        'qt__output_distribution': ['uniform', 'normal'],
-        'qt__ignore_implicit_zeros': [False],
-        'qt__subsample': [int(1e4), int(1e5), int(1e6)],
-        'sp__percentile': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
-        'svc__C': [0.001, 0.01, 0.1, 1.0, 10, 100, 1000],
-        'svc__kernel': ['rbf'],
+        'qt__n_quantiles': [50, 100, 150, 1000],
+        #'qt__output_distribution': ['uniform'],
+        #'qt__ignore_implicit_zeros': [False],
+        #'qt__subsample': [int(1e4), int(1e5), int(1e6)],
+        'sk__k': [500, 750, 1000],
+        #'sp__percentile': [70, 100],
+        'svc__C': [0.1, 1.0, 10],
+        #'svc__kernel': ['rbf'],
         'svc__gamma': ['scale'],
-        'svc__shrinking': [True, False],
-        'svc__probability': [True, False],
-        'svc__class_weight': [None, 'balanced'],
-        'svc__decision_function_shape': ['ovr', 'ovo']
+        #'svc__shrinking': [True],
+        'svc__probability': [True],
+        'svc__class_weight': ['balanced'],
+        #'svc__decision_function_shape': ['ovr']
 
     }
 
@@ -89,7 +92,7 @@ def evaluate(X_train, y_train, iid):
         iid=iid,
         cv=5,
         refit=False,
-        verbose=0,
+        verbose=2,
         error_score='raise',
         return_train_score=False,
     )
@@ -109,7 +112,7 @@ def evaluate(X_train, y_train, iid):
     print("======================================================================================")
 
 
-def predict(X_train, y_train, X_test, class_weights):
+def predict(X_train, y_train, X_test):
     score = make_scorer(balanced_accuracy_score, greater_is_better=True)
 
     skfold = StratifiedKFold(n_splits=5, shuffle=False, random_state=42)
@@ -118,14 +121,17 @@ def predict(X_train, y_train, X_test, class_weights):
         C=1.0,
         kernel='rbf',
         gamma='scale',
-        class_weight=class_weights,
+        shrinking=True,
+        probability=True,
+        class_weight='balanced',
         verbose=False,
         max_iter=-1,
         decision_function_shape='ovr',
+
     )
 
     results = cross_val_score(svc, X_train, y_train, cv=skfold, n_jobs=-1, scoring=score)
-    print("Results: %.2f (%.2f) MSE" % (results.mean(), results.std()))
+    print("Results: %.4f (%.4f) MSE" % (results.mean(), results.std()))
     svc.fit(X_train, y_train)
     return svc.predict(X_test)
 
@@ -135,17 +141,14 @@ def main():
     X_train, X_test, y_train, test_index = read_data()
     y_train = np.squeeze(y_train)
 
-    for iid in [True, False]:
-        evaluate(X_train, y_train, iid)
+    #evaluate(X_train, y_train, True)
 
+    X_train, X_test = scale_features(X_train, X_test)
+    X_train, X_test = remove_unimportant_features(X_train, X_test, y_train)
+    y_pred = predict(X_train, y_train, X_test)
+    hf.count_class_occurences(y_pred)
+    print("Counter({1.0: 3075, 0.0: 512.5, 2.0: 512.5})  Should be!")
 
-    # X_train, X_test = scale_features(X_train, X_test)
-    # X_train, X_test = remove_unimportant_features(X_train, X_test, y_train)
-    # y_pred = predict(X_train, y_train, X_test, class_weights)
-    # hf.count_class_occurences(y_pred)
-    # print("Counter({1.0: 3075, 0.0: 512.5, 2.0: 512.5})  Should be!")
-    #
-    # hf.write_to_csv_from_vector("output_franz.csv", test_index, y_pred, "id")
-
+    hf.write_to_csv_from_vector("output_franz.csv", test_index, y_pred, "id")
 
 main()
